94e36babcfd097c55ac2a97ed823531855dd5a67
mr_undercross <mrUndercross@gmail.com>
2025-07-12 19:17:00 +0300

    Вернул как было

--- news/urls.py
+++ news/urls.py
@@ -3,6 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.news_home, name='news_home'),
-    path('create', views.create_post, name='create_post')
+    path('create', views.create_post, name='create_post'),
+    path('<int:pk>', views.NewsDetailView.as_view(), name='news_detail'),
+    path('<int:pk>/update', views.NewsUpdateView.as_view(), name='news_update'),
+    path('<int:pk>/delete', views.NewsDeleteView.as_view(), name='news_delete')
+
 
 ]
--- news/views.py
+++ news/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render, redirect
+from django.views.generic import DetailView, UpdateView, DeleteView
 from .models import Articles
 from .forms import ArticlesForm
+
+
+class NewsDeleteView(DeleteView):
+    model = Articles
+    template_name = 'news/delete_post_page.html'
+    success_url = '/news/'
+
+
+class NewsUpdateView(UpdateView):
+    model = Articles
+    template_name = 'news/create_post_page.html'
+    form_class = ArticlesForm
+
+
+class NewsDetailView(DetailView):
+    model = Articles
+    template_name = 'news/news_detail.html'
+    context_object_name = 'article'
 
 
 def news_home(request):
