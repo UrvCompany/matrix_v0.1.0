698e8ee1b7c27c0ef67812e86927c2c866c7aaf7
mr_undercross <mrUndercross@gmail.com>
2025-07-12 16:34:49 +0300

    A горький вкус твоей любви

--- news/urls.py
+++ news/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.news_home, name='news_home'),
+    path('create', views.create_post, name='create_post')
 
 ]
--- news/views.py
+++ news/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Articles
+from .forms import ArticlesForm
 
 
 def news_home(request):
@@ -8,3 +9,26 @@ def news_home(request):
     # news = Articles.objects.order_by('-date')[:1]      Доп фишки
 
     return render(request, 'news/news_home.html', {'news': news})
+
+
+def create_post(request):
+
+    error = ''
+    if request.method == 'POST':
+        form = ArticlesForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('news_home')
+        else:
+            error = 'Ты что творишь, шелуха?'
+
+    form = ArticlesForm()
+    data = {
+        'form': form,
+        'error': error,
+    }
+
+    return render(request, 'news/create_post_page.html', data)
+
+
+
